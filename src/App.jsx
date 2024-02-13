e6bd197ec6d5ce4cf62d242029cb365981be2e2d
muditkalra <muditkalra267@gmail.com>
2024-02-13 17:39:19 +0530

    link created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
     <div> Lorem ipsum dolor sit amet consectetur.</div>
+    <Link to={'/home'}> Home</Link>
       <Routes>
         <Route path="/home" element={<Home />} />
       </Routes>
