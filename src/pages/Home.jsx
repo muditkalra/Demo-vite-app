b79d033b46c98de5f3c6d43373e38bc078f845b1
muditkalra <muditkalra267@gmail.com>
2024-02-14 13:11:19 +0530

    back link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../App.css'
+import { Link } from 'react-router-dom'
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
 
     return (
         <>
+        <Link to={'/'}>back</Link>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
